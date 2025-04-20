2749695d93d9097103736b1ff568d668b3fed880
priyesh-xX <priyesh12479@gmail.com>
2025-04-20 11:43:33 +0530

    Tailwind changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>Gnosis App</h1>
+    <div className="w-full h-screen bg-zinc-900 ">
+      <h1 className="text-4xl font-bold text-blue-500">Let's GO</h1>
+      <h2 className="text-2xl font-bold text-blue-500">Gnosis</h2>
     </div>
   );
-};
+}
 
 export default App;
